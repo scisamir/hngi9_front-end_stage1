1cf5083efa0656f50f4dfc5db9a73316d30b4ab3
Idris Samir <samzidris11@gmail.com>
2022-11-01 07:57:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import me from "./me.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="linktree">
+        <img src={me} alt="my photo" id="profile_img" />
+        <h2 id="name">Samir Idris</h2>
+        <a href="https://twitter.com/ScientistSamir" id="twitter">Twitter Link</a>
+        <p hidden id="slack">scisamir</p>
+
+        <a href="https://training.zuri.team/" id="btn_zuri">Zuri Team</a>
+        <a href="http://books.zuri.team" id="books" title="This is where you find books about design and coding">Zuri Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=scisamir" id="book_python" title="This is a concise and great book to help you understand python as fast as possible">Python Books</a>
+        <a href="https://background.zuri.team" id="pitch" title="For many years, HNG has been graduating qualfied and experienced coders and designers">Background Check for Coders</a>
+        <a href="https://books.zuri.team/design-rules" id="book_design" title="This book guides you to make you a great designer">Design Books</a>
+      </div>
     </div>
   );
 }
